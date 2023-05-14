498512d64db12214b1bd9f9d85f52da4f80d7587
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-15 00:29:05 +0300

    creating header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './nullstyle.css';
 
+import Header from './components/header/Header';
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      
+    </header>
+  );
+}
+
+export default Header;
