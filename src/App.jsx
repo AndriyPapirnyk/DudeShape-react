75160441a495e337ac1175ffec3058b1ded59791
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-16 23:28:30 +0300

    gulpfile for scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css';
 import './nullstyle.css';
 
-import Header from './components/header/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <header className="main">
+        <div className="main__container">
+            
+        </div>
+    </header>
+  );
+}
+
+export default Main;
