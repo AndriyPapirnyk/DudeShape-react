3dd53be5462be47f8dd10e1ba5d507a27bf4d710
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-17 00:17:01 +0300

    final home block version

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import './Main.css';
 import Header from './header/Header';
+import Home from './home/Home';
 
 function Main() {
   return (
     <div className="main">
         <div className="main__container">
             <Header />
+            <Home />
         </div>
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,17 @@
 import './Home.css';
+import chairImg from './img/home__chair.jpg'
 
 function Home() {
   return (
-    <div className="main">
-        
+    <div className="home">
+        <img src={chairImg} alt="" />
+        <div className="home__main">
+            <div>
+            <h1>We Help You Make <br/> Modern Furniture</h1>
+            <p>All of our furniture uses the best materials and <br /> choices for our customers.All of our furniture uses <br /> the best materials </p>
+            <button>Explore More</button>
+            </div>
+        </div>
     </div>
   );
 }
