da698037ac92d9457350f52f5ae7b4e35f53d9ec
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-19 23:07:49 +0300

    popular furniture component final version

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,15 +1,16 @@
 import './Popular.css';
 import Card from './card/Card';
+import nextImg from './img/next-prev.png'
 
 function Popuplar() {
   return (
     <section className='popular'>
       <h1>Our Popular Furniture</h1>
       <div className="row">
-        <p></p>
+        <p>All of our furniture uses the best materials and choices for our customers.All of our <br /> furniture uses the best materials and choices for our customers.</p>
         <div className='row__options'>
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src={nextImg} alt="" />
+          <img src={nextImg} alt="" />
         </div>
       </div>
       <div className="popular__list">
