de7361f98b01481cf3cc079da53efc100a32a8f8
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-19 22:58:20 +0300

    card component creation

--- src/components/popular/card/Card.jsx
+++ src/components/popular/card/Card.jsx
@@ -1,9 +1,26 @@
 import './Card.css';
+import likeImg from './img/like.png'
+import shareImg from './img/share.png'
+import mainImg from './img/main.png'
+
 
 function Card() {
   return (
     <div className="card">
-        hello
+        <img src={mainImg} alt="" />
+        <div className="card__info">
+            <div className="row">
+                <h1>White Swan Chair </h1>
+                <div>
+                    <img src={likeImg} alt="" />
+                    <img src={shareImg} alt="" />
+                </div>
+            </div>
+            <div className="row">
+                <p>$40</p>
+                <button>Buy Now</button>
+            </div>
+        </div>
     </div>
   );
 }
