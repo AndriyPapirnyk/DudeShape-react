3514244e97b04bd08167a073c999c9685117cb41
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-19 22:58:03 +0300

    card component creation

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -4,7 +4,19 @@ import Card from './card/Card';
 function Popuplar() {
   return (
     <section className='popular'>
-       <Card />
+      <h1>Our Popular Furniture</h1>
+      <div className="row">
+        <p></p>
+        <div className='row__options'>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="popular__list">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </section>
   );
 }
