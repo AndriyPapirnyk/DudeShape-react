c2803c26adafd207439ce02d9130f9a4b0b073fa
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-23 19:03:50 +0300

    subsription block final versoin

--- src/components/subsription/Subscription.jsx
+++ src/components/subsription/Subscription.jsx
@@ -13,8 +13,8 @@ function Subscription() {
   return (
     <section className='subscription'>
       <div className="subscription__main">
-        <h1>f</h1>
-        <p>f</p>
+        <h1>Subscribe to get the latest news <br /> about us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br /> eiusmod tempor incididunt ut labore at dolore.</p>
         {inputs.map(item => <Input key={item.buttonText} placeholder={item.placeholder} img={item.img} buttonText={item.buttonText} />)}
       </div>
     </section>
--- src/components/subsription/input/Input.jsx
+++ src/components/subsription/input/Input.jsx
@@ -4,8 +4,10 @@ import './Input.css';
 function Input(props) {
   return (
     <div className='input'>
+        <div>
         <img src={props.img} alt="" />
         <input type="email" placeholder={props.placeholder} />
+        </div>
         <button>{props.buttonText}</button>
     </div>
   );
