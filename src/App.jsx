3b8fa73bdd7758b513ca12ccd09e78a99f38faf3
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-25 00:30:42 +0300

    adding footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './nullstyle.css';
-
 import Main from './components/Main';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+        <div className="footer__container">
+        <div className="footer__main">
+
+        </div>
+        <div className="footer__copyright"></div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
