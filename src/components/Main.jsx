66b58690fdec936b3ac7bc76a7d6c463d884dfaf
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-28 16:17:44 +0300

    final version of adaptive for furniture block

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import Home from './home/Home';
 import Companies from './companies/Companies';
 import About from './about/About';
 import Popuplar from './popular/Popular';
-// import Furniture from './furniture/Furniture';
+import Furniture from './furniture/Furniture';
 // import Customers from './customers/Customers';
 // import Subscription from './subsription/Subscription';
 
@@ -17,7 +17,7 @@ function Main() {
             <Companies />
             <About />
             <Popuplar />
-            {/* <Furniture /> */}
+            <Furniture />
             {/* <Customers />  */}
             {/* <Subscription /> */}
         </div>
